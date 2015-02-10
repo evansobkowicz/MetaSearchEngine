04269ca23dd72a772e276d43a47bbdb94f32e2f8
Evan Sobkowicz <evansobkowicz@gmail.com>
2015-02-10 10:04:08 -0500

    bug fixes and comments

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from WebDB import *
 from google import search
 from spider import *
 
-
+# Get item types from file names in item directory
 def get_item_types():
     types = list()
     path = "data/item/"
@@ -17,17 +17,19 @@ def get_item_types():
         types.append(type)
     return types
 
+# Read items from text file into a list
 def get_items_by_type(type):
     results = list()
     path = "data/item/" + type + ".txt"
     f = open(path, "r")
     lines = f.readlines()
     for line in lines:
-        cleanline = line.strip('\n')
-        results.append(cleanline)
+        clean_line = line.strip('\n')
+        results.append(clean_line)
     f.close()
     return results
 
+# Write 'contents' to a file named 'id' in the folder 'dir'
 def write_to_file(id, dir, contents):
     file_path = "data/" + dir + "/" + str(id) + ".txt"
     f = open(file_path, "w", encoding='utf-8')
@@ -44,6 +46,7 @@ def write_to_file(id, dir, contents):
         print('ERROR: Could not write to', dir, 'file!')
     f.close()
 
+# Create directories if they don't exist
 def setup_directories():
     directories = ["clean", "header", "item", "raw"]
     for dir in directories:
@@ -51,16 +54,22 @@ def setup_directories():
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
+# Main Loop
+# Google, Parse, Add to DB, and Cache
 def main():
 
+    # Initialize
     count = 0
     spider = Spider()
+    google_results = 10
 
-    # Setup the Cache Structure
+    # Setup the Cache File Structure
     setup_directories()
+
+    # Setup Database
     db = WebDB("data/cache.db")
 
-    # For each list of items      movies, books, music artists
+    # For each list of items (movies, books, music)
     for type in get_item_types():
 
         print(type)
@@ -68,38 +77,42 @@ def main():
         # For each item in the list
         for item in get_items_by_type(type):
 
-            print(' ')
-            print(item)
+            print("\n", item)
 
+            # Add the item to the database
             item_id = db.insertItem(db._quote(item), type)
 
             # Use Search Engine to find N=10 relevant URLs
             # For each URL
-            for url in search(str(item), stop=10, pause=0.5):
+            for url in search(str(item), stop=google_results, pause=0.5):
 
                 print("\t " + url)
 
+                # Check if url is already cached
                 if db.lookupCachedURL_byURL(url):
                     print("\t\t Already Cached!")
                 else:
 
-                    # Download, Parse, & Cache Each Website     # if not already in cache
+                    # Download, Parse, & Cache Each Website
                     response = spider.fetch(url)
 
+                    # Process the Spider's response
                     if response == -1:
+                        # Failure in the Spider's fetching
                         print('ERROR: Spider fetch failure!')
                     else:
+                        # Successful Spider response
                         page_title = db._quote(response[0])
                         headers = "Title: " + page_title + "\n" + str(response[1])
                         terms = response[2] # These are stemmed!
                         html = response[3]
-                        doc_type = None
+                        doc_type = response[4]
 
-                        # Update information in cache database
+                        # Update information in database
                         url_id = db.insertCachedURL(url, doc_type, page_title)
                         url_to_item_id = db.insertURLToItem(url_id, item_id)
 
-                        # Write files
+                        # Write cache files
                         write_to_file(url_id, "header", headers)
                         write_to_file(url_id, "raw", html)
                         write_to_file(url_id, "clean", terms)
@@ -108,6 +121,7 @@ def main():
                         print('\t\t Added Successfully!')
                         count += 1
 
+    # Print the number of processed URLs
     print("Processed " + str(count) + " URLs.")
 
 
--- spider.py
+++ spider.py
@@ -8,47 +8,71 @@ from bs4 import BeautifulSoup
 import bs4
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
-
+import re
 
 class Spider:
 
     def fetch(self, url):
+
+        # Request the page, catch errors to prevent program from crashing by returning '-1' back to the main
+        response = self.request(url)
+        if response == -1:
+            return -1
+
+        # Read and parse the response using BeautifulSoup
+        html = response.read().decode('utf-8', errors='ignore')
+        soup = BeautifulSoup(html)
+        [x.extract() for x in soup.findAll('script')] # Strip all script tags
+
+        # Get Title, with error handling for no head, and no title
+        if (soup.html is not None) and (soup.html.head is not None) and (soup.html.head.title is not None):
+            title = soup.html.head.title.get_text()
+        else:
+            title = ""
+
+        # Get Body, with error handling for no body
+        if (soup.html is not None) and (soup.html.body is not None):
+            body = soup.html.body.get_text()
+        else:
+            body = ""
+
+        # Process text: Tokenize, Unique Terms, Lowercase Terms, Stemmed Terms
+        tokens = word_tokenize(body)
+        terms = self.get_terms(tokens)
+        lower_terms = self.get_terms(self.lower(terms))
+        stem_terms = self.get_terms(self.stem(lower_terms))
+
+        # Return data for main
+        return title, response.info(), stem_terms, str(soup), self.doctype(response.info())
+
+    # Get the page, or return '-1' on an error
+    def request(self, url):
         req = Request(url, None, {'User-agent': 'Firefox/3.05'})
         try:
-            response = urlopen(req)
+            return urlopen(req)
         except (HTTPError, URLError) as error:
             print('ERROR:  Could not retrieve', url, 'because', error)
             return -1
         except timeout:
             print('ERROR:  Socket timed out - URL: %s', url)
             return -1
-        html = response.read().decode('utf-8', errors='ignore')
-        soup = BeautifulSoup(html)
-        if (soup.html is not None) and (soup.html.head is not None) and (soup.html.head.title is not None):
-            title = soup.html.head.title.get_text()
-        else:
-            title = ""
-        [x.extract() for x in soup.findAll('script')]
-        if (soup.html is not None) and (soup.html.body is not None):
-            body = soup.html.body.get_text()
-        else:
-            body = ""
-        tokens = word_tokenize(body)
-        terms = self.get_terms(tokens)
-        lower_terms = self.get_terms(self.lower(terms))
-        stem_terms = self.get_terms(self.stem(lower_terms))
-        return title, response.info(), stem_terms, str(soup), self.doctype(soup)
 
-    def doctype(self, soup):
-        items = [item for item in soup.contents if isinstance(item, bs4.Doctype)]
-        return items[0] if items else None
+    # Get the doctype from the headers
+    def doctype(self, headers):
+        match = re.search("content-type:\s*([\w/]+);", str(headers), re.IGNORECASE)
+        try:
+            return match.group(1)
+        except:
+            return None
 
+    # Lowercase a list of tokens
     def lower(self, tokens):
         lower_tokens = list()
         for w in tokens:
             lower_tokens.append(w.lower())
         return lower_tokens
 
+    # Stem a list of tokens
     def stem(self, tokens):
         stemmed_tokens = list()
         ps = PorterStemmer()
@@ -56,6 +80,7 @@ class Spider:
             stemmed_tokens.append(ps.stem(word))
         return stemmed_tokens
 
+    # Get unique tokens
     def get_terms(self, tokens):
         terms = list()
         for word in tokens:
